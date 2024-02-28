ae35d5c4f825fe6b873992b72bba6353a29c4c7f
Supriya-Morapelli <suppusupriyareddy123@gmail.com>
2024-02-28 22:15:30 +0530

    first message

---
+++ src/Streams.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+public class Streams {
+	//@Test
+	public void regular()
+	{
+		ArrayList<String> names=new ArrayList<String>();
+		names.add("Anusha");
+		names.add("Ashritha");
+		names.add("Supriya");
+		names.add("Amani");
+		names.add("Priya");
+		int count=0;
+		for(int i=0;i<names.size();i++) {
+		String actual=names.get(i);
+		if(actual.startsWith("A")) {
+			count++;
+		}
+		System.out.println(count);
+		}}
+		@Test
+		public void streamFilter()
+		{
+			ArrayList<String> names=new ArrayList<String>();
+			names.add("Anusha");
+			names.add("Ashritha");
+			names.add("Supriya");
+			names.add("Amani");
+			names.add("Priya");
+		Long c=	names.stream().filter(s->s.startsWith("A")).count();
+		System.out.println(c);
+		}
+		
+	}
+
+
+
+
